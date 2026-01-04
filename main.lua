cf6542a974493c6aabc8b5531aa0aa33db35ce0f
ProudFridge <proudfridge@gmail.com>
2026-01-04 17:06:45 -0500

    Fixed my implementation of the euler method, added trails to each planet

--- main.lua
+++ main.lua
@@ -3,35 +3,53 @@ local Timer = require("timer")
 local Utils = require("utils")
 local Gravity = require("gravity")
 
-
 local planetList = {}
-local timer = Timer.new(0)
+local timer = Timer.new(1)
 
 local debug = false
-local simulation = true
+local simulation = false
 local clear = false
 
-local spawnGrid = true
+local spawnGrid = false
 
 -- local constant = 6.6743e-11
 local constant = 3
-local scale = 1/2
+local scale = 2
+local checks = 0
+local time = 0
 
--- local constant = 4
+local color = {1,1,1}
+
+function love.load()
+    --Initial planets to see if the simulation even works
+    local centerX = love.graphics.getWidth() / 2
+    local centerY = love.graphics.getHeight() / 2 
+
+    table.insert(planetList, Planet:new(color, nil, 5, 2, centerX, centerY, -0.3, 3))
+    table.insert(planetList, Planet:new(color, nil, 10, 2, centerX + 8, centerY + 6, 0, 0))
+end
+
 function love.update(dt)
+    -- dt = 0.0005
+    
     if love.keyboard.isDown("escape") then
         love.event.quit()
     end
 
+    checks = 0
+
+    --Spawns a grid of planets -> need to fix their position(preferably find a better way to switch between scree nadn global coordinates)
     if spawnGrid == true then
-        local max = 10
+        local max = 20
+        local seperation = 100
         local color = {1,1,1}
         local newPosX = 0
         local newPosY = 0
+        scale = 1/seperation * max
         for i = 1, max do
             for j = 1, max do
-                newPosX = 1000 * i/max
-                newPosY = 1000 * j/max
+                newPosX = seperation * max * i/max
+                newPosY = seperation * max * j/max
                 table.insert(planetList, Planet:new(color, nil, 100000, nil, newPosX * 1/scale, newPosY * 1/scale))
             end
         end
@@ -39,9 +57,7 @@ function love.update(dt)
     end
 
     timer:tick(dt)
-    local checks = 0
-    local color = {1,1,1}
-
+    
     --Insert new planet
     if love.keyboard.isDown("w") and timer.isDone == true then
         local newPosX = love.mouse.getX() * (1/scale)
@@ -62,39 +78,44 @@ function love.update(dt)
     end
 
     if simulation then
-        --Reset acceleration of each planet
-        for i, planet in ipairs(planetList) do
-            planet.acceleration_x = 0
-            planet.acceleration_y = 0
-        end
-
-        --Calculate the acceleration and speed of each lanet
+        time = time + dt
+        --Calculate the acceleration and speed of each planet
         for i = 1, #planetList do
-            for j = i + 1, #planetList do
-                local planet1 = planetList[i]
-                local planet2 = planetList[j]
-                local force = Gravity.computeGravitationalForce(planet1, planet2, constant)
-                Gravity.computeVelocity(planet1, planet2, dt, force)
+            for j = 1, #planetList do
+                if j ~= i then
+                    local planet1 = planetList[i]
+                    local planet2 = planetList[j]
+                    local force = Gravity.computeGravitationalForce(planet1, planet2, constant)
+                    Gravity.computeVelocity(planet1, planet2, dt, force)
 
-                checks = checks + 1
+                    checks = checks + 1
+                end
             end
         end
 
         for i, planet in ipairs(planetList) do
-            Gravity.applyGravity(planet, dt)
+            Gravity.advancePosition(planet, dt)
+            planet:insertTrailPoint(200)
         end
     end
 
     if debug == true then
-        print(#planetList)
         print(checks)
     end
 end
 
 -- Draws everything
 function love.draw()
+    love.graphics.setColor(1,1,1 )
+    love.graphics.print(string.format("%f bodies", #planetList), 0,0)
+    love.graphics.print(string.format("%f checks", checks), 0, 12)
+    love.graphics.print(string.format("%f seconds", time), 0, 24)
+
     love.graphics.push()
     love.graphics.scale(scale, scale)
+
+    love.graphics.setLineWidth(1)
+    Planet.renderTrails(planetList)
 
     for i,planet in ipairs(planetList) do
         planet:render()
@@ -106,7 +127,6 @@ function love.draw()
         Planet.renderLines(planetList, constant, scale)
     end
 end
-
 
 function love.keypressed(key)
     if key == "e" then
--- planet.lua
+++ planet.lua
@@ -4,7 +4,7 @@ Planet.__index = Planet
 local Utils = require("utils")
 local Gravity = require("gravity")
 
-function Planet:new(color, radius, mass, density, pos_x, pos_y, velocity_x, velocity_y, acceleration_x, acceleration_y)
+function Planet:new(color, radius, mass, density, pos_x, pos_y, velocity_x, velocity_y, acceleration_x, acceleration_y, pointList)
     local newPlanet = {}
     setmetatable(newPlanet, Planet)
 
@@ -24,6 +24,8 @@ function Planet:new(color, radius, mass, density, pos_x, pos_y, velocity_x, velo
 
     newPlanet.acceleration_x = acceleration_x or 0
     newPlanet.acceleration_y = acceleration_y or 0
+
+    newPlanet.pointList = {newPlanet.pos_x, newPlanet.pos_y, newPlanet.pos_x, newPlanet.pos_y}
 
     return newPlanet
 end
@@ -55,7 +57,7 @@ function Planet.renderLines(planetList, constant, scale)
             --     love.graphics.line(planet1.pos_x, planet1.pos_y, planet2.pos_x, planet2.pos_y)
             -- end
 
-            if distance < 800 * scale then
+            if distance < 200 * 1/scale then
                 --Prints the distance between two planet at the midpoint of the line
                 love.graphics.setColor(1, 1, 1, 400 / distance * 1/scale)
                 love.graphics.print(string.format("%.2fm",distance), x_component / 2 + planet1.pos_x * scale, y_component / 2 + planet1.pos_y * scale, math.atan2(y_component, x_component), scale, nil, 0, 25)
@@ -71,6 +73,25 @@ function Planet.renderLines(planetList, constant, scale)
         end
     end
 end
+
+function Planet:insertTrailPoint(maxPoints)
+    --Removes points when they reach a certain threshold
+    if #self.pointList > maxPoints then
+        table.remove(self.pointList, 1)
+        table.remove(self.pointList, 1)
+    end
+
+    --Inserts a new point each frame
+    table.insert(self.pointList, self.pos_x)
+    table.insert(self.pointList, self.pos_y)
+end
+
+function Planet.renderTrails(planetList)
+    for i,planet in ipairs(planetList) do
+        love.graphics.line(planet.pointList)
+    end
+end
+
 
 function Planet:printInfo()
     print("Color: ", self.r, self.g, self.b, "\nRadius: ", self.radius, "\nMass: ", self.mass, "\nDensity: ", self.density, "\nPositionX: ", self.pos_x, "\nPositionY: ", self.pos_y)
